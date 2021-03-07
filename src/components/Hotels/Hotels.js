ad9d22b81ea9ba7b5fa6203988d59f63ff16d173
Agnieszka Polińska <agapoli@gmail.com>
2021-03-07 15:33:43 +0100

    first commit

---
+++ src/components/Hotels/Hotels.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Hotel from './Hotel';
+import styles from './Hotels.module.css';
+
+class Hotels extends Component {
+    render() {
+        return (
+            <div className={styles.container}>
+                <h2 className={styles.title}>Oferty</h2>
+                <Hotel />
+                <Hotel />
+                <Hotel />
+            </div>
+        )
+    }
+}
+
+/* function Hotels() {
+    return (
+        <div>
+            Hotele
+        </div>
+    )
+} */
+
+export default Hotels;
