6e2aa74b7d9ceedc17992961ed79ef81fedc40ce
Agnieszka Polińska <agapoli@gmail.com>
2021-03-07 19:51:33 +0100

    Style info about hotel

--- src/components/Hotels/Hotel.js
+++ src/components/Hotels/Hotel.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import styles from './Hotel.module.css';
+import hotelImg from '../../assets/images/hotel.jpg'
 
 function Hotel() {
     return (
-        <div className={styles.hotel}>Hotel</div>
+        <div className={`row ${styles.hotel}`}>
+            <div className="col-4">
+                <img 
+                    src={hotelImg}
+                    alt=""
+                    className="img-fluid"
+                />
+            </div>
+            <div className="col-8">
+                <div className="row">
+                    <div className="col">
+                        <p>Hotel</p>
+                        <p>Miasto</p>
+                    </div>
+                    <div className="col">
+                        <p>Ocena: 8.4</p>
+                        <p>Opinie: 215</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-12">
+                <p>Fuga nihil quae, aperiam dolorem repudiandae voluptatibus incidunt eaque maiores voluptate iusto quo explicabo culpa alias dolorum accusamus corporis quibusdam ipsum iure.</p>
+                {/* eslint-disable-next-line */}
+                <a href="#" className="btn btn-primary">Pokaż</a>
+            </div>
+        </div>
     )
 }
 
