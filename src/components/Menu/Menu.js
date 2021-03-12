0946be40323b2af053e66dd7812e62e29d1aa5f8
Agnieszka Polińska <agapoli@gmail.com>
2021-03-12 15:33:44 +0100

    Remove container css class

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,7 +2,7 @@ import styles from './Menu.module.css';
 
 function Menu() {
     return (
-        <div className={`${styles.menuContainer} container`}>
+        <div className={styles.menuContainer}>
             <ul className={styles.menu}>
                 <li className={styles.menuItem}>
                 {/* eslint-disable-next-line */}
