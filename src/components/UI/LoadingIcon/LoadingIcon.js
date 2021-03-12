a4487c367eb8aa51329d8c61b2520bbcc5c4c7e7
Agnieszka Polińska <agapoli@gmail.com>
2021-03-12 10:06:53 +0100

    Add loading bar

---
+++ src/components/UI/LoadingIcon/LoadingIcon.js
@@ -0,0 +1,11 @@
+function LoadingIcon() {
+    return (
+        <div className="d-flex justify-content-center">
+            <div className="spinner-border m-5" role="status">
+                <span className="sr-only">Ładowanie...</span>
+            </div>
+        </div>
+    )
+}
+
+export default LoadingIcon
