51c4c50e895e35a5a99105d35ff59981fb77093f
Agnieszka Polińska <agapoli@gmail.com>
2021-03-12 06:38:55 +0100

    Get props from App.js

--- src/components/Hotels/Hotel.js
+++ src/components/Hotels/Hotel.js
@@ -1,8 +1,7 @@
-import React from 'react';
 import styles from './Hotel.module.css';
 import hotelImg from '../../assets/images/hotel.jpg'
 
-function Hotel() {
+function Hotel(props) {
     return (
         <div className={`card ${styles.hotel}`}>
             <div className="card-body">
@@ -17,18 +16,18 @@ function Hotel() {
                     <div className="col-8">
                         <div className="row">
                             <div className="col">
-                                <p className={styles.title}>Hotel</p>
-                                <span className="badge badge-light">Wrocław</span>
+                                <p className={styles.title}>{props.name}</p>
+                                <span className="badge badge-light">{props.city}</span>
                             </div>
                             <div className="col text-right">
-                                <h5>Ocena: 8.4</h5>
+                                <h5>{props.rating}</h5>
                                 {/* eslint-disable-next-line */}  
                                 <a href="#" className="btn btn-primary float-right mt-2 px-5">Pokaż</a>
                             </div>
                         </div>
                     </div>
                     <div className="col-12">
-                        <p className={styles.description}>Fuga nihil quae, aperiam dolorem repudiandae voluptatibus incidunt eaque maiores voluptate iusto quo explicabo culpa alias dolorum accusamus corporis quibusdam ipsum iure.</p>
+                        <p className={styles.description}>{props.description}</p>
                     </div>
                 </div>
             </div>
