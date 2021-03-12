2a0b3dc8caaa3e21ea89c695cbf02ee221ef8ed6
Agnieszka Polińska <agapoli@gmail.com>
2021-03-12 12:31:09 +0100

    Remove import of Searchbar.js to Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,9 @@
 import styles from './Header.module.css';
-import Searchbar from './Searchbar/Searchbar';
 
 function Header(props) {
     return (
-        <header className={`${styles.header} container`}>
-            <Searchbar onSearch={props.onSearch}/>
+        <header className={`${styles.header} container`}> 
+            {props.children}  
         </header>
     );
 }
