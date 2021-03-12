9f39b05324ea59747e8ac7bdb311559b3f220ae9
Agnieszka Polińska <agapoli@gmail.com>
2021-03-12 15:35:26 +0100

    Change notation of colors

--- src/components/Hotels/Hotel.js
+++ src/components/Hotels/Hotel.js
@@ -30,7 +30,7 @@ function Hotel(props) {
                             <div className="col text-right">
                                 <h5>{props.rating}</h5>
                                 {/* eslint-disable-next-line */}  
-                                <a href="#" className="btn btn-primary float-right mt-2 px-5">Pokaż</a>
+                                <a href="#" className={`btn btn-${props.theme} float-right mt-2 px-5`}>Pokaż</a>
                             </div>
                         </div>
                     </div>
