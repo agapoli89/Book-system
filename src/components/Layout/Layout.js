323ccb6d6352e6147f2b75e06390d419343770d4
Agnieszka Polińska <agapoli@gmail.com>
2021-03-14 07:29:09 +0100

    Back to Layout.js uses withClass.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,13 +3,13 @@ import withClass from '../../hoc/withClass';
 
 function Layout(props) {
     return (
-        <Fragment className='layout'>
+        <>
             <div>{props.header}</div>
             <div className="container">{props.menu}</div>
             <div className="container">{props.content}</div>
             <div>{props.footer}</div>
-        </Fragment>
+        </>
     )
 }
 
-export default Layout;
+export default withClass(Layout, 'layout');
