fcc02180e279a623612e8d4baa6231a77d61e473
Agnieszka Polińska <agapoli@gmail.com>
2021-03-18 18:40:23 +0100

    useDebugValue to check auth in developer tool

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useDebugValue } from 'react';
 import AuthContext from '../context/authContext'
 
 export default function useAuth() {
     const authContext = useContext(AuthContext);
 
     const auth = authContext.isAuthenticated;
+
+    //useDebugValue(auth ? 'Zalogowany' : 'Wylogowany')
+
     const setAuth = (value) => {
         if (value) {
             authContext.login();
