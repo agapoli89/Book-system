3c93a7a835eedd7b2c49e403745bc2ea9dd185f5
Agnieszka Polińska <agapoli@gmail.com>
2021-03-23 16:53:20 +0100

    Implement LastHotel logic

--- src/components/Hotels/LastHotel/LastHotel.js
+++ src/components/Hotels/LastHotel/LastHotel.js
@@ -1,5 +1,19 @@
 function LastHotel(props) {
-    return ();
+    
+    return (
+        <div className='card bg-light'>
+            <div className="card-header">Ostatnio oglądałaś/eś ten hotel. Czy nadal zainteresowana/y?</div>
+            <h5 className='card-title text-center mt-3'>{props.name}</h5>
+            <div className='card-body d-flex justify-content-center'>
+                <a href="#" className="btn btn-sm btn-dark mx-2">Tak</a>
+                <button 
+                    onClick={props.onRemove} 
+                    className="btn btn-sm btn-dark mx-2">
+                    Nie
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default LastHotel;
