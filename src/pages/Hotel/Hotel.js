a34b168aa6d98457a6d74768f1e46781dd7e8584
Agnieszka Polińska <agapoli@gmail.com>
2021-03-26 07:17:24 +0100

    Create one hotel page

--- src/pages/Hotel/Hotel.js
+++ src/pages/Hotel/Hotel.js
@@ -1,12 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import LoadingIcon from "../../components/UI/LoadingIcon/LoadingIcon";
 
 function Hotel(props) {
+    const { id } = useParams();
+    const [hotel, setHotel] = useState({});
+    const [loading, setLoading] = useState(true);
+    
+    const fetchHotel = () => {
+        setHotel({
+            id: 1,
+            name: 'Przy ratuszu',
+            city: 'Wrocław',
+            rating: 8.4,
+            description: 'Fuga nihil quae, aperiam dolorem repudiandae voluptatibus incidunt eaque maiores voluptate iusto quo explicabo culpa alias dolorum accusamus corporis quibusdam ipsum iure.',
+            image: ''
+          });
+        setLoading(false);
+    }
 
     useEffect(() => {
-
+        setTimeout(() => {
+            fetchHotel();
+        }, 500);
     }, []);
 
-    return <h1>To jest jakiś hotel</h1>;
+    if (loading) return <LoadingIcon />
+
+    return <h1>Hotel: {hotel.name}</h1>;
 }
 
 export default Hotel;
