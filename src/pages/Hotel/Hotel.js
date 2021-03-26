24d06fc012daad46f3f1fb343a4e967ade7263cc
Agnieszka Polińska <agapoli@gmail.com>
2021-03-26 09:09:00 +0100

    Remove reducer

--- src/pages/Hotel/Hotel.js
+++ src/pages/Hotel/Hotel.js
@@ -1,11 +1,11 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import ReducerContext from "../../context/reducerContext";
+import LoadingIcon from "../../components/UI/LoadingIcon/LoadingIcon";
 
 function Hotel(props) {
     const { id } = useParams();
-    const [hotel, setHotel] = useState({});
-    const reducer = useContext(ReducerContext);
+    const [hotel, setHotel] = useState(null);
+    const [loading, setLoading] = useState(true);
     
     const fetchHotel = () => {
         
@@ -17,19 +17,18 @@ function Hotel(props) {
             description: 'Fuga nihil quae, aperiam dolorem repudiandae voluptatibus incidunt eaque maiores voluptate iusto quo explicabo culpa alias dolorum accusamus corporis quibusdam ipsum iure.',
             image: ''
           });
-          reducer.dispatch({ type: 'set-loading', loading: false })
+          setLoading(false);
     }
 
     useEffect(() => {
-        reducer.dispatch({ type: 'set-loading', loading: true })
         setTimeout(() => {
             fetchHotel();
         }, 500);
     }, []);
 
-    if (reducer.state.loading) return null;
-
-    return <h1>Hotel: {hotel.name}</h1>;
+    return loading ? <LoadingIcon /> : (
+        <h1>Hotel: {hotel.name}</h1>
+    );
 }
 
 export default Hotel;
