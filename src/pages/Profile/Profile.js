4c07ccae7d0c8c6db7fcc3d16debdd05bae6ab5f
Agnieszka Polińska <agapoli@gmail.com>
2021-03-27 07:03:53 +0100

    Implement logic in Profile.js

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,32 @@
+import { Switch, Route, NavLink, useRouteMatch } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails/ProfileDetails';
+import MyHotels from './MyHotels/MyHotels';
+
 export default function Profile(props) {
+    const { path, url } = useRouteMatch();
+
     return (
-        <div>
-            <h2>Mój Profil</h2>
+        <div className="card">
+            <div className="card-header">
+                <h2>Mój Profil</h2>
+            </div>
+            <div className="card-body">
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" exact to={`${url}`}>Profil</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to={`${url}/hotele`}>Hotele</NavLink>
+                    </li>
+                </ul>
+
+                <div className="pt-4">
+                    <Switch>
+                        <Route path={`${path}/hotele`} component={MyHotels}/>
+                        <Route path={`${path}`} component={ProfileDetails}/>
+                    </Switch>
+                </div>
+            </div>
         </div>
     )
 }
