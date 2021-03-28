28100082dd84b3ec662fb4c9b79f996844123fbc
Agnieszka Polińska <agapoli@gmail.com>
2021-03-28 08:03:00 +0200

    Form control

--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -1,15 +1,44 @@
+import { useState } from "react";
+import useAuth from '../../../hooks/useAuth';
+import { useHistory } from 'react-router-dom';
+
 export default function Login(props) {
+    const [auth, setAuth] = useAuth();
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [passowrd, setPassowrd] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const submit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        
+        setTimeout(() => {
+            //logowanie
+            setAuth(true);
+            history.push('/');
+        }, 500);
+    }
+
     return (
         <div>
             <h2>Logowanie</h2>
-            <form>
+            <form onSubmit={submit}>
                 <div className="form-group">
                     <label>Email</label>
-                    <input type="email" className="form-control"/>
+                    <input 
+                        type="email" 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                        className="form-control"/>
                 </div>
                 <div className="form-group">
                     <label>Hasło</label>
-                    <input type="password" className="form-control"/>
+                    <input 
+                        type="password" 
+                        value={passowrd} 
+                        onChange={e => setPassowrd(e.target.value)} 
+                        className="form-control"/>
                 </div>
                 <button className="btn btn-primary">Zapisz</button>
             </form>
