c21072e0926f504dde1faf26be37d6094ada3520
Agnieszka Polińska <agapoli@gmail.com>
2021-04-02 19:01:00 +0200

    Add the type of error to error message

--- src/hoc/ErrorBoundary.js
+++ src/hoc/ErrorBoundary.js
@@ -3,10 +3,11 @@ import { Component } from 'react';
 class ErrorBoundary extends Component {
     state = {
         hasError: false,
+        error: null
     };
 
     static getDerivedStateFromError(error) {
-        return { hasError: true };
+        return { hasError: true, error };
     }
 
     componentDidCatch(error, errorInfo) {
@@ -15,7 +16,7 @@ class ErrorBoundary extends Component {
 
     render() {
         if (this.state.hasError) {
-            return <div className="alert alert-danger">Wystąpił jakiś problem.</div>
+            return <div className="alert alert-danger">Wystąpił jakiś problem {this.state.error.message}</div>
         }
         return this.props.children;
     }
