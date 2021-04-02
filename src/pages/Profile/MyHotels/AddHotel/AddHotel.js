bc1bcd6aaaeb7876b0b046da845af01be9dbf18b
Agnieszka Polińska <agapoli@gmail.com>
2021-04-02 17:29:03 +0200

    Assign values of the errors

--- src/pages/Profile/MyHotels/AddHotel/AddHotel.js
+++ src/pages/Profile/MyHotels/AddHotel/AddHotel.js
@@ -1,9 +1,9 @@
 import { useRef, useState } from "react";
 import LoadingButton from "../../../../components/UI/LoadingButton/LoadingButton";
 import Input from '../../../../components/Input/Input';
+import { validate } from '../../../../helpers/validations'
 
 function AddHotel(props) {
-    const imageRef = useRef();
     const [form, setForm] = useState({
         name: {
             value: '',
@@ -57,7 +57,17 @@ function AddHotel(props) {
         }, 500);
     }
 
-    const changeHandler = (value, fieldName) => setForm({...form, [fieldName]: {...form[fieldName], value} });
+    const changeHandler = (value, fieldName) => {
+        setForm({
+            ...form, 
+            [fieldName]: {
+                ...form[fieldName], 
+                value,
+                showError: true,
+                error: 'Pole wymagane'
+            } 
+        });
+    }
 
     return (
         <div className="card">
@@ -71,23 +81,23 @@ function AddHotel(props) {
                         label="Nazwa"
                         value={form.name.value}
                         onChange={val => changeHandler(val, 'name')}
-                        isValid={true}
-                        showError={false}/>
+                        error={form.name.error}
+                        showError={form.name.showError}/>
 
                     <Input 
                         label="Opis"
                         type="textarea"
                         value={form.description.value}
                         onChange={val => changeHandler(val, 'description')}
-                        error=""
-                        showError={false}/>
+                        error={form.description.error}
+                        showError={form.description.showError}/>
                     
                     <Input 
                         label="Miejscowość"
                         value={form.city.value}
                         onChange={val => changeHandler(val, 'city')}
-                        error=""
-                        showError={false}/>
+                        error={form.city.error}
+                        showError={form.city.showError}/>
 
                     <Input 
                         label="Ilość pokoi"
@@ -100,8 +110,8 @@ function AddHotel(props) {
                             { value: 3, label: 3},
                             { value: 4, label: 4},
                         ]}
-                        error=""
-                        showError={false}/>     
+                        error={form.rooms.error}
+                        showError={form.rooms.showError}/>     
 
                     <h4>Udogodnienia</h4>
                     <Input 
@@ -113,13 +123,15 @@ function AddHotel(props) {
                             { value: 'wifi', label: 'WiFi' },
                             { value: 'parking', label: 'Parking' },
                         ]}
-                        error=""
-                        showError={false}/>
+                        error={form.features.error}
+                        showError={form.features.showError}/>
 
                     <h4>Zdjęcie</h4>
                     <Input 
                         type="file"
-                        onChange={val => changeHandler(val, 'image')}/>
+                        onChange={val => changeHandler(val, 'image')}
+                        error={form.image.error}
+                        showError={form.image.showError}/>
 
                     <h4>Status</h4>
                     <Input 
@@ -131,8 +143,8 @@ function AddHotel(props) {
                             { value: '1', label: 'Aktywny' },
                             { value: '0', label: 'Ukryty' },
                         ]}
-                        error=""
-                        showError={false}/>
+                        error={form.status.error}
+                        showError={form.status.showError}/>
 
                     <div className="text-right">
                         <LoadingButton loading={loading} className="btn-success">
