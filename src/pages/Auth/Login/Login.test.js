d30e2758a43908cc02d837d1dda2a080c9f804d6
Agnieszka Polińska <agapoli@gmail.com>
2021-04-15 06:26:31 +0200

    Automatic test: changes email value

--- src/pages/Auth/Login/Login.test.js
+++ src/pages/Auth/Login/Login.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Login from './Login'; 
 
 test('renders Logowanie', () => {
     render(<Login />);
     expect(screen.getByText(/logowanie/i)).toBeInTheDocument();
 });
+
+test('changes email value', () => {
+    const utils = render(<Login />);
+    const emailInput = utils.getByLabelText('Email');
+
+    fireEvent.change(emailInput, { target: { value: 'agapoli'} });
+    expect(emailInput.value).toBe('agapoli');
+})
 
