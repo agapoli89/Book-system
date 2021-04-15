b196c6d190a7ea2e2ca2439de83691cd24a8bbf9
Agnieszka Polińska <agapoli@gmail.com>
2021-04-15 07:15:42 +0200

    Automatic test: show error on fail login

--- src/pages/Auth/Login/Login.test.js
+++ src/pages/Auth/Login/Login.test.js
@@ -1,5 +1,17 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Login from './Login'; 
+import axios from '../../../axios-auth';
+
+jest.mock('axios', () => {
+    return {
+        create: () => {
+            return {
+                post: jest.fn(() => Promise.resolve('')),
+                get: jest.fn(() => Promise.resolve('')),
+            }
+        }
+    }
+});
 
 test('renders Logowanie', () => {
     render(<Login />);
@@ -12,5 +24,20 @@ test('changes email value', () => {
 
     fireEvent.change(emailInput, { target: { value: 'agapoli'} });
     expect(emailInput.value).toBe('agapoli');
+});
+
+test('show error on fail login', async () => {
+    axios.post.mockImplementation(() => 
+        Promise.reject({ response: {data: {error: {message: 'Błędne dane'}}}})
+    )
+
+    const utils = render(<Login />);
+    const submitButton = utils.getByText('Zaloguj');
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(axios.post).toHaveBeenCalledTimes(1));
+
+    expect(screen.getByText('Błędne dane')).toBeInTheDocument();
 })
 
